c217ecc82720f65acfff15d19f16c0fe93e5eef5
tthost <daniel@tthost.com.br>
2020-07-25 21:53:36 -0300

    indentação

--- aula-11/aula/js/script.js
+++ aula-11/aula/js/script.js
@@ -1,101 +1,101 @@
-window.addEventListener('load', () => {
-  doMap();
-  doFilter();
-  doForEach();
-  doReduce();
-  doFind();
-  doSome();
-  doEvery();
-  doSort();
+window.addEventListener("load", () => {
+    doMap();
+    doFilter();
+    doForEach();
+    doReduce();
+    doFind();
+    doSome();
+    doEvery();
+    doSort();
 });
 
 function doMap() {
-  const nameEmailArray = people.results.map(person => {
-    return {
-      name: person.name,
-      email: person.email
-    };
-  });
+    const nameEmailArray = people.results.map((person) => {
+        return {
+            name: person.name,
+            email: person.email
+        };
+    });
 
-  console.log(nameEmailArray);
+    console.log(nameEmailArray);
 
-  return nameEmailArray;
+    return nameEmailArray;
 }
 
 function doFilter() {
-  const olderThan50 = people.results.filter(person => {
-    return person.dob.age > 50;
-  });
+    const olderThan50 = people.results.filter((person) => {
+        return person.dob.age > 50;
+    });
 
-  console.log(olderThan50);
+    console.log(olderThan50);
 }
 
 function doForEach() {
-  const mappedPeople = doMap();
+    const mappedPeople = doMap();
 
-  mappedPeople.forEach(person => {
-    person.nameSize =
-      person.name.title.length +
-      person.name.first.length +
-      person.name.last.length;
-  });
+    mappedPeople.forEach((person) => {
+        person.nameSize =
+            person.name.title.length +
+            person.name.first.length +
+            person.name.last.length;
+    });
 
-  console.log(mappedPeople);
+    console.log(mappedPeople);
 }
 
 function doReduce() {
-  const totalAges = people.results.reduce((accumulator, current) => {
-    return accumulator + current.dob.age;
-  }, 0);
+    const totalAges = people.results.reduce((accumulator, current) => {
+        return accumulator + current.dob.age;
+    }, 0);
 
-  console.log(totalAges);
+    console.log(totalAges);
 
-  // let sumAges = 0;
+    // let sumAges = 0;
 
-  // for (let i = 0; i < people.results.length; i++) {
-  //   var current = people.results[i];
-  //   sumAges += current.dob.age;
-  // }
+    // for (let i = 0; i < people.results.length; i++) {
+    //   var current = people.results[i];
+    //   sumAges += current.dob.age;
+    // }
 
-  // console.log(sumAges);
+    // console.log(sumAges);
 }
 
 function doFind() {
-  const found = people.results.find(person => {
-    return person.location.state === 'Minas Gerais';
-  });
+    const found = people.results.find((person) => {
+        return person.location.state === "Minas Gerais";
+    });
 
-  console.log(found);
+    console.log(found);
 }
 
 function doSome() {
-  const found = people.results.some(person => {
-    return person.location.state === 'Amazonas';
-  });
+    const found = people.results.some((person) => {
+        return person.location.state === "Amazonas";
+    });
 
-  console.log(found);
+    console.log(found);
 }
 
 function doEvery() {
-  const every = people.results.every(person => {
-    return person.nat === 'BR';
-  });
+    const every = people.results.every((person) => {
+        return person.nat === "BR";
+    });
 
-  console.log(every);
+    console.log(every);
 }
 
 function doSort() {
-  const mappedNames = people.results
-    .map(person => {
-      return {
-        name: person.name.first
-      };
-    })
-    .filter(person => person.name.startsWith('A'))
-    .sort((a, b) => {
-      return b.name.length - a.name.length;
-      // return a.name.localeCompare(b.name);
-    });
+    const mappedNames = people.results
+        .map((person) => {
+            return {
+                name: person.name.first
+            };
+        })
+        .filter((person) => person.name.startsWith("A"))
+        .sort((a, b) => {
+            return b.name.length - a.name.length;
+            // return a.name.localeCompare(b.name);
+        });
 
-  console.log(mappedNames);
+    console.log(mappedNames);
 }
