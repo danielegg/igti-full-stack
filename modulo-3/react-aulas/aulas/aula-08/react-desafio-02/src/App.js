98717e009cd7790b5680e2182226d7b359fcb2f4
tthost <daniel@tthost.com.br>
2020-08-15 19:15:10 -0300

    .

--- modulo-3/react-aulas/aulas/aula-08/react-desafio-02/src/App.js
+++ modulo-3/react-aulas/aulas/aula-08/react-desafio-02/src/App.js
@@ -3,91 +3,95 @@ import Countries from './components/countries/Countries';
 import Header from './components/header/Header';
 
 export default class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      allCountries: [],
-      filteredCountries: [],
-      filteredPopulation: 0,
-      filter: '',
+        this.state = {
+            allCountries: [],
+            filteredCountries: [],
+            filteredPopulation: 0,
+            filter: '',
+        };
+    }
+
+    async componentDidMount() {
+        const res = await fetch('https://restcountries.eu/rest/v2/all');
+        const json = await res.json();
+
+        const allCountries = json.map(
+            ({ name, numericCode, flag, population }) => {
+                return {
+                    id: numericCode,
+                    name,
+                    filterName: name.toLowerCase(),
+                    flag,
+                    population,
+                };
+            }
+        );
+
+        const filteredPopulation = this.calculateTotalPopulationFrom(
+            allCountries
+        );
+
+        this.setState({
+            allCountries,
+            filteredCountries: Object.assign([], allCountries),
+            filteredPopulation,
+        });
+    }
+
+    calculateTotalPopulationFrom = (countries) => {
+        const totalPopulation = countries.reduce((accumulator, current) => {
+            return accumulator + current.population;
+        }, 0);
+
+        return totalPopulation;
     };
-  }
 
-  async componentDidMount() {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const json = await res.json();
+    handleChangeFilter = (newText) => {
+        this.setState({
+            filter: newText,
+        });
 
-    const allCountries = json.map(({ name, numericCode, flag, population }) => {
-      return {
-        id: numericCode,
-        name,
-        filterName: name.toLowerCase(),
-        flag,
-        population,
-      };
-    });
+        const filterLowerCase = newText.toLowerCase();
 
-    const filteredPopulation = this.calculateTotalPopulationFrom(allCountries);
+        const filteredCountries = this.state.allCountries.filter((country) => {
+            return country.filterName.includes(filterLowerCase);
+        });
 
-    this.setState({
-      allCountries,
-      filteredCountries: Object.assign([], allCountries),
-      filteredPopulation,
-    });
-  }
+        const filteredPopulation = this.calculateTotalPopulationFrom(
+            filteredCountries
+        );
 
-  calculateTotalPopulationFrom = (countries) => {
-    const totalPopulation = countries.reduce((accumulator, current) => {
-      return accumulator + current.population;
-    }, 0);
+        this.setState({
+            filteredCountries,
+            filteredPopulation,
+        });
+    };
 
-    return totalPopulation;
-  };
+    render() {
+        const { filteredCountries, filter, filteredPopulation } = this.state;
 
-  handleChangeFilter = (newText) => {
-    this.setState({
-      filter: newText,
-    });
+        return (
+            <div className="container">
+                <h1 style={styles.centeredTitle}>React Countries</h1>
 
-    const filterLowerCase = newText.toLowerCase();
+                <Header
+                    filter={filter}
+                    countryCount={filteredCountries.length}
+                    totalPopulation={filteredPopulation}
+                    onChangeFilter={this.handleChangeFilter}
+                />
 
-    const filteredCountries = this.state.allCountries.filter((country) => {
-      return country.filterName.includes(filterLowerCase);
-    });
-
-    const filteredPopulation = this.calculateTotalPopulationFrom(
-      filteredCountries
-    );
-
-    this.setState({
-      filteredCountries,
-      filteredPopulation,
-    });
-  };
-
-  render() {
-    const { filteredCountries, filter, filteredPopulation } = this.state;
-
-    return (
-      <div className="container">
-        <h1 style={styles.centeredTitle}>React Countries</h1>
-
-        <Header
-          filter={filter}
-          countryCount={filteredCountries.length}
-          totalPopulation={filteredPopulation}
-          onChangeFilter={this.handleChangeFilter}
-        />
-
-        <Countries countries={filteredCountries} />
-      </div>
-    );
-  }
+                <Countries countries={filteredCountries} />
+            </div>
+        );
+    }
 }
 
 const styles = {
-  centeredTitle: {
-    textAlign: 'center',
-  },
+    centeredTitle: {
+        textAlign: 'center',
+    },
 };
--- modulo-3/react-aulas/aulas/aula-08/react-desafio-02/src/components/countries/Countries.js
+++ modulo-3/react-aulas/aulas/aula-08/react-desafio-02/src/components/countries/Countries.js
@@ -4,15 +4,15 @@ import Country from './Country';
 import css from './countries.module.css';
 
 export default class Countries extends Component {
-  render() {
-    const { countries } = this.props;
+    render() {
+        const { countries } = this.props;
 
-    return (
-      <div className={`${css.border} ${css.flexRow}`}>
-        {countries.map((country) => {
-          return <Country key={country.id} country={country} />;
-        })}
-      </div>
-    );
-  }
+        return (
+            <div className={`${css.border} ${css.flexRow}`}>
+                {countries.map((country) => {
+                    return <Country key={country.id} country={country} />;
+                })}
+            </div>
+        );
+    }
 }
